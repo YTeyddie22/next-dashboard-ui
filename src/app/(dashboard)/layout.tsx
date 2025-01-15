0392f6bc4e7af7e4e0de724e5c0fb260e8d4aa1c
YTeyddie22 <teyddie@gmail.com>
2025-01-15 12:11:57 +0300

    feat: implement the responsive navbar section

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import Image from "next/image";
 import Menu from "@/components/Menu";
+import Navbar from "@/components/Navbar";
 
 export default function DashboardLayout({
 	children,
@@ -18,8 +19,9 @@ export default function DashboardLayout({
 				</Link>
 				<Menu />
 			</div>
-			<div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[96%]">
-				Right
+			<div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[96%] bg-[#F7F8FA] overflow-scroll">
+				<Navbar />
+				{children}
 			</div>
 		</div>
 	);
