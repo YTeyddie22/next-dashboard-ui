df7a293266ea9409da677d9bf969b66ed0974fe3
YTeyddie22 <teyddie@gmail.com>
2025-01-17 11:33:16 +0300

    create cards

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,7 +1,20 @@
+import UserCards from "@/components/UserCards";
 import React from "react";
 
 const AdminPage = () => {
-	return <div>AdminPage</div>;
+	return (
+		<div className="p-4 flex flex-col gap-4 md:flex-row">
+			<div className="w-full lg:w-2/3">
+				<div className="flex gap-4 justify-between flex-wrap">
+					<UserCards type="admin" />
+					<UserCards type="teacher" />
+					<UserCards type="parent" />
+					<UserCards type="student" />
+				</div>
+			</div>
+			<div className="w-full lg:w-1/3"></div>
+		</div>
+	);
 };
 
 export default AdminPage;
