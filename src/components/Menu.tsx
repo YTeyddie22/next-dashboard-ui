ecfbc1e884d072a9a02dda5ed081752ca9cf8bda
YTeyddie22 <teyddie@gmail.com>
2025-01-24 11:11:43 +0300

    use dummy data

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import menuItems from "@/components/MenuData";
 import Link from "next/link";
 import Image from "next/image";
+import { role } from "@/lib/data";
 
 const Menu = () => {
 	return (
@@ -11,22 +12,26 @@ const Menu = () => {
 					<span className="hidden lg:block text-gray-400 font-light my-4">
 						{items.title}
 					</span>
-					{items.items.map((item) => (
-						<Link
-							href={item.href}
-							key={item.label}
-							className="flex items-center justify-center lg:justify-start gap-4 text-gray-500 py-2">
-							<Image
-								src={item.icon}
-								alt="icon"
-								width={20}
-								height={20}
-							/>
-							<span className="hidden lg:block">
-								{item.label}
-							</span>
-						</Link>
-					))}
+					{items.items.map((item) => {
+						if (item.visible.includes(role)) {
+							return (
+								<Link
+									href={item.href}
+									key={item.label}
+									className="flex items-center justify-center lg:justify-start gap-4 text-gray-500 py-2 md:px-2 hover:bg-tedPurpleLight">
+									<Image
+										src={item.icon}
+										alt="icon"
+										width={20}
+										height={20}
+									/>
+									<span className="hidden lg:block">
+										{item.label}
+									</span>
+								</Link>
+							);
+						}
+					})}
 				</div>
 			))}
 		</div>
