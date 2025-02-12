6eedf7480fc91bea05ad776619fad6f08621c35c
YTeyddie22 <teyddie@gmail.com>
2025-02-12 07:45:40 +0300

    feat: ui for parents, teachers and students

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -19,7 +19,7 @@ export default function DashboardLayout({
 				</Link>
 				<Menu />
 			</div>
-			<div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[96%] bg-[#F7F8FA] overflow-scroll">
+			<div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[96%] bg-[#F7F8FA] overflow-scroll flex flex-col">
 				<Navbar />
 				{children}
 			</div>
